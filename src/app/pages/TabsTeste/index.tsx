7dcfd26f29e585f453bfa90d8b4ee73d622de087
Rogerio Pavan <rogerio_pavan@hotmail.com>
2020-11-12 10:29:51 -0300

    feat: faz demo de tabs bootstrap

--- src/app/pages/TabsTeste/index.tsx
+++ src/app/pages/TabsTeste/index.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
-import { Nav, Tab, Tabs } from 'react-bootstrap'
-import { Route } from 'react-router-dom'
+import { Nav, Tab } from 'react-bootstrap'
 import './styles.scss'
 
 interface TabProps {
