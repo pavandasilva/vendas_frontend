cc7f7e6c924a51ea942b53a7572cf31cb66db5bb
Rogerio Pavan <rogerio_pavan@hotmail.com>
2020-11-16 12:19:52 -0300

    feat: adiciona loading

--- src/app/components/Clientes/index.tsx
+++ src/app/components/Clientes/index.tsx
@@ -13,12 +13,15 @@ export const Clientes = () => {
   const [search, setSearch] = useState('')
   const [currentPage, setCurrentPage] = useState(1)
   const [numberRows, setNumberRows] = useState(0)
+  const [loading, setLoading] = useState(false)
+  const perPage = useMemo(() => 10, [])
   const { data } = useUsuario()
   const { addTab } = useTabs()
-  const perPage = useMemo(() => 2, [])
 
   useEffect(() => {
     async function fetch () {
+      setLoading(true)
+
       const response = await trazerClientesFidelizados.execute(
         data?.funcionario_id as unknown as number,
         data?.token as string,
@@ -29,12 +32,13 @@ export const Clientes = () => {
 
       setNumberRows(response.metadata.count)
       setClientesFidelizados(response?.data as Cliente[])
+      setLoading(false)
     }
 
     fetch()
   }, [currentPage, data, perPage, search])
 
-  const handleAtenderOnClick = (cliente: Cliente) => {
+  const handleAtenderOnClick = useCallback((cliente: Cliente) => {
     if (!cliente?.id) {
       return
     }
@@ -44,7 +48,7 @@ export const Clientes = () => {
       title: `${cliente.id} - ${cliente.nome_fantasia}`,
       content: <Atendimento cliente={cliente}/>
     })
-  }
+  }, [addTab])
 
   const searchOnChange = useCallback((event: ChangeEvent<HTMLInputElement>) => {
     setSearch(event.target.value)
@@ -64,43 +68,49 @@ export const Clientes = () => {
       <div className="card-body">
         <h2>Clientes</h2>
         <input onChange={searchOnChange}></input>
-        <Table striped bordered hover size="sm">
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Razão</th>
-              <th>UF</th>
-              <th>Cidade</th>
-              <th>Ações</th>
-            </tr>
-          </thead>
-          <tbody>
-            { clientesFidelizados?.map(cliente => (
-              <tr key={cliente?.id?.toString()}>
-                <td>{cliente?.id}</td>
-                <td>{capitalize(cliente?.razao_social as string)}</td>
-                <td>{cliente?.uf?.toUpperCase()}</td>
-                <td>{capitalize(cliente?.cidade as string)}</td>
-                <td><button onClick={() => handleAtenderOnClick(cliente)}>Atender</button></td>
-              </tr>
-            ))}
-          </tbody>
-        </Table>
+        { loading ? <h1>loading...</h1> : (
+          <>
+            <Table striped bordered hover size="sm">
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Razão</th>
+                  <th>UF</th>
+                  <th>Cidade</th>
+                  <th>Ações</th>
+                </tr>
+              </thead>
+              <tbody>
+                { clientesFidelizados?.map(cliente => (
+                  <tr key={cliente?.id?.toString()}>
+                    <td>{cliente?.id}</td>
+                    <td>{capitalize(cliente?.razao_social as string)}</td>
+                    <td>{cliente?.uf?.toUpperCase()}</td>
+                    <td>{capitalize(cliente?.cidade as string)}</td>
+                    <td><button onClick={() => handleAtenderOnClick(cliente)}>Atender</button></td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
 
-        {
-          numberRows > perPage && (
-            <Pagination>
-              <Pagination.First onClick={() => handlePaginationOnClick(1)}/>
-              <Pagination.Prev onClick={() => handlePaginationOnClick(currentPage - 1)}/>
+            {
+              numberRows > perPage && (
+                <Pagination>
+                  <Pagination.First onClick={() => handlePaginationOnClick(1)}/>
+                  <Pagination.Prev onClick={() => handlePaginationOnClick(currentPage - 1)}/>
 
-              { Array.apply(0, Array(Math.ceil(numberRows / perPage))).map((x, i) => {
-                return <Pagination.Item active={currentPage === i + 1} onClick={() => handlePaginationOnClick(i + 1)} key={i.toString()}>{i + 1}</Pagination.Item>
-              })}
-              <Pagination.Next onClick={() => handlePaginationOnClick(currentPage + 1)}/>
-              <Pagination.Last onClick={() => handlePaginationOnClick(Math.ceil(numberRows / perPage))}/>
-            </Pagination>
-          )
-        }
+                  { Array.apply(0, Array(Math.ceil(numberRows / perPage))).map((_, i) =>
+                    <Pagination.Item active={currentPage === i + 1} onClick={() => handlePaginationOnClick(i + 1)} key={i.toString()}>{i + 1}</Pagination.Item>
+                  )}
+                  <Pagination.Next onClick={() => handlePaginationOnClick(currentPage + 1)}/>
+                  <Pagination.Last onClick={() => handlePaginationOnClick(Math.ceil(numberRows / perPage))}/>
+                </Pagination>
+              )
+            }
+          </>
+
+        )}
+
       </div>
     </div>
   )
