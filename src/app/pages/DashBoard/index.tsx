3b8c1f04d29bee3c2cc67b9236c28b985694b8e7
Leandro <leandrocorrea@live.com>
2020-11-26 09:26:51 -0300

    feat: alteração na tela do atendimento

--- src/app/pages/DashBoard/index.tsx
+++ src/app/pages/DashBoard/index.tsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import { Layout } from '../Layout'
 import { ListaClientesFidelizados } from '../../components'
+import { Container } from 'react-bootstrap'
 
 export const DashBoard: React.FC = () => {
   return (
     <Layout title="Dashboard">
-      <div id="container" className="container-fluid">
-        <ListaClientesFidelizados />
-      </div>
+      <ListaClientesFidelizados />
     </Layout>
   )
 }
