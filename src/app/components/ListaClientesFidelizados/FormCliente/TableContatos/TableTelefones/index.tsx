b3088489a563555a3e1f8b33212182de80fbcfba
Rogerio Pavan <rogerio_pavan@hotmail.com>
2020-11-27 17:49:35 -0300

    feat: adiciona botao para criar telefone de contato

--- src/app/components/ListaClientesFidelizados/FormCliente/TableContatos/TableTelefones/index.tsx
+++ src/app/components/ListaClientesFidelizados/FormCliente/TableContatos/TableTelefones/index.tsx
@@ -1,15 +1,37 @@
-import React from 'react'
+import React, { ChangeEvent, useEffect, useState } from 'react'
 import { Table } from 'react-bootstrap'
-import { Contato } from '../../../../../../domain/clientes/models'
+import { Contato, Telefone } from '../../../../../../domain/clientes/models'
 
 interface TableTelefonesProps {
   contato: Contato
 }
 
 export const TableTelefones = ({ contato }: TableTelefonesProps) => {
+  const [telefones, setTelefones] = useState([] as Telefone [])
+
+  useEffect(() => {
+    if (contato?.telefones?.length) {
+      setTelefones(contato.telefones)
+    }
+  }, [contato])
+
+  const handleOnClickNovoTelefone = () => {
+    setTelefones(telefones => {
+      const emptyTelefone: Telefone = {
+        ddd: '',
+        e_whatsapp: false,
+        numero: '',
+        ramal: ''
+      }
+
+      const newTelefones = [...telefones, emptyTelefone]
+      return newTelefones
+    })
+  }
+
   return (
     <>
-      <button>Novo</button>
+      <button onClick={handleOnClickNovoTelefone} >Novo</button>
       <Table bordered hover size="sm">
         <thead>
           <tr>
@@ -20,25 +42,16 @@ export const TableTelefones = ({ contato }: TableTelefonesProps) => {
           </tr>
         </thead>
         <tbody>
-          { contato?.telefones?.map((telefone, index) =>
-            <tr key={index.toString()}>
-              <td>{telefone.ddd}</td>
-              <td>{ telefone.numero}</td>
-              <td>{telefone.ramal}</td>
-              <td>{telefone.e_whatsapp}</td>
-            </tr>)
-          }
-
-          {/* { contato?.telefones?.map(telefone =>
+          { telefones?.length ? telefones.map((telefone, index) =>
             (
-              <tr>
+              <tr key={index.toString()}>
                 <td>{telefone.ddd}</td>
                 <td>{ telefone.numero}</td>
                 <td>{telefone.ramal}</td>
                 <td>{telefone.e_whatsapp}</td>
               </tr>
-            )
-          } */}
+            )) : <h3>Nenhum telefone cadastrado</h3>
+          }
         </tbody>
       </Table>
     </>
